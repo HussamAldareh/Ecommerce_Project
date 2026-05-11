71d56f645d7b53d234df0d13549c50f2b1a1eb57
HussamAldareh <hussamaldareh095@gmail.com>
2026-05-11 11:03:15 +0300

    inital commit

---
+++ Ecommerce_Project/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecommerce_Project.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<Product>? Products { get; set; }
+
+    }
+}
---
+++ Ecommerce_Project/Models/Discount.cs
@@ -0,0 +1,23 @@
+﻿namespace Ecommerce_Project.Models
+{
+    public class Discount
+    {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Percentage { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public decimal MinmumAmount { get; set; }
+
+    }
+}
